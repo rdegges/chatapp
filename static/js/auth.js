213353b74c4a690a59d456c03197c1b518aa22f5
Randall Degges <r@rdegges.com>
2018-04-05 12:30:21 -0700

    Moving JS into auth.js.

--- static/js/auth.js
+++ static/js/auth.js
@@ -22,3 +22,53 @@ function showLogin() {
 
   document.getElementById("login").style.display = "none";
 }
+
+// Determine whether or not we have a querystring.
+function hasQuerystring() {
+  return location.href.indexOf("?") !== -1;
+}
+
+// Handle the user's login and what happens next.
+function handleLogin() {
+  // If the user is logging in for the first time...
+  if (okta.token.hasTokensInUrl()) {
+    okta.token.parseTokensFromUrl(
+      function success(res) {
+        // Save the tokens for later use, e.g. if the page gets refreshed:
+        okta.tokenManager.add("accessToken", res[0]);
+        okta.tokenManager.add("idToken", res[1]);
+
+        // Redirect to this user's dedicated room URL.
+        window.location = getRoomURL();
+      }, function error(err) {
+        alert("We weren't able to log you in, something horrible must have happened. Please refresh the page.");
+      }
+    );
+  } else {
+    okta.session.get(function(res) {
+
+      // If the user is logged in, display the app.
+      if (res.status === "ACTIVE") {
+
+        // If the user is logged in on the home page, redirect to their room page.
+        if (location.href.indexOf("?") === -1) {
+          window.location = getRoomURL();
+        }
+
+        return enableVideo();
+      }
+
+      // If we get here, the user is not logged in.
+
+      // If there's a querystring in the URL, it means this person is in a
+      // "room" so we should display our passive login notice. Otherwise,
+      // we'll prompt them for login immediately.
+      if (hasQuerystring()) {
+        document.getElementById("login").style.display = "block";
+        enableVideo();
+      } else {
+        showLogin();
+      }
+    });
+  }
+}
