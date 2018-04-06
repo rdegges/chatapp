f47c439b4de77cf09abaafd9d6c6d6d079738c69
Randall Degges <r@rdegges.com>
2018-04-05 21:07:54 -0700

    Fixing some broken helper code.

--- src/static/js/video.js
+++ src/static/js/video.js
@@ -1,6 +1,12 @@
 // Determine the room name and public URL for this chat session.
 function getRoom() {
-  return (location.search && decodeURIComponent(location.search.split("?")[1].split("=")[1])) || okta.tokenManager.get("idToken").claims.email;
+  var query = location.search && location.search.split("?")[1];
+
+  if (query) {
+    return (location.search && decodeURIComponent(query.split("=")[1]));
+  }
+
+  return okta.tokenManager.get("idToken").claims.email;
 }
 
 // Retrieve the absolute room URL.
